457f26433115b1c87f133d187b16e00999716e36
Max Emil Schoen <9975295+maxemil@users.noreply.github.com>
2023-06-23 14:03:10 +0200

    Add heatmaps

--- make_abundance_plot.py
+++ make_abundance_plot.py
@@ -13,7 +13,15 @@ df_min = df[(df.sum(axis=1) > 0.1)]
 df_min.loc['Other'] = df[(df.sum(axis=1) <= 0.1)].sum()
 df_min = df_min*100
 
-df_min.to_excel('Fungal_orders_Nothofagus.xlsx', header=True, index=True)
+# df_min.to_excel('Fungal_orders_Nothofagus.xlsx', header=True, index=True)
+
+fig, ax = plt.subplots(figsize=(12,10))
+sns.heatmap(df_min, annot=True, cmap=sns.color_palette("Blues", as_cmap=True), fmt=".1f", ax=ax)
+ax.xaxis.tick_top()
+ax.set_ylabel(f"{tax_level.capitalize()}")
+ax.set_xlabel("Relative abundance [%]")
+plt.tight_layout()
+plt.savefig(f'Fungal_{tax_level}_heatmap.pdf')
 
 colors = sns.color_palette("hls", df_min.shape[0]-1)
 colors.append('gray')
